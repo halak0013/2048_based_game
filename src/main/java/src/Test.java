6441ff282324706b0cceda242ec1af9122125b84
halak0013 <halakmuhammet145@gmail.com>
2023-03-25 22:50:23 +0300

    testing list

---
+++ src/main/java/src/Test.java
@@ -0,0 +1,20 @@
+package src;
+
+public class Test {
+    public static void main(String[] args) {
+        MultiDimList<Integer> l=new MultiDimList<>();
+        l.addProces(2, 0);
+        l.addProces(4, 0);
+        l.addProces(4, 0);
+        l.addProces(8, 1);
+        l.addProces(8, 1);
+        l.addProces(8, 1);
+        l.addProces(16, 2);
+        l.addProces(16, 2);
+        l.addProces(8, 2);
+        l.addProces(16, 2);
+        l.addProces(8, 1);
+        l.addProces(8, 4);
+
+    }
+}
