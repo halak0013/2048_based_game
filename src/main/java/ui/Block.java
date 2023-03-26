469d01934cbb1d3e46eccecf3d0418ed0cf6fed1
halak0013 <halakmuhammet145@gmail.com>
2023-03-26 16:56:59 +0300

    add change text function

--- src/main/java/ui/Block.java
+++ src/main/java/ui/Block.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
- */
 package ui;
 
 /**
@@ -10,13 +6,13 @@ package ui;
  */
 public class Block extends javax.swing.JPanel {
 
-    /**
-     * Creates new form block
-     */
     public Block() {
         initComponents();
     }
 
+    public void changeVal(String valv){
+        this.val.setText(valv);
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
